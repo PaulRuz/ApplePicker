656a804667638d1695ef302c4202e76824d8619e
Pavel Ruzanov <ruzanov@yahoo.com>
2019-12-09 17:00:40 +0300

    Creating AppleTree and Apple behaviour

---
+++ Prototype/Assets/Scripts/Apple.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour
+{
+    private static float bottomY = -10f;
+
+    void Update()
+    {
+        Vector2 currentPosition = transform.position;
+        if (currentPosition.y <= bottomY)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Prototype/Assets/Scripts/AppleTree.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppleTree : MonoBehaviour
+{
+    public GameObject applePrefab = null;
+    public float periodCreateApple = 1.2f;
+
+    private float speedMove = 4f;
+    private float distanceChangeDirection = 6f;
+    private float chanceChangeDirection = 0.01f;
+
+    private void Start()
+    {
+        RecursiveCreateApple();
+    }
+
+    private void Update()
+    {
+        Vector2 currentPosition = transform.position;
+        currentPosition.x += speedMove * Time.deltaTime;
+        transform.position = currentPosition;
+
+        if (currentPosition.x <= -distanceChangeDirection)
+        {
+            speedMove = Mathf.Abs(speedMove);
+        }
+        else if (currentPosition.x >= distanceChangeDirection)
+        {
+            speedMove = -Mathf.Abs(speedMove);
+        }
+        if (Random.value < chanceChangeDirection)
+        {
+            speedMove *= -1;
+        }
+    }
+
+    private void RecursiveCreateApple()
+    {
+        Instantiate(applePrefab, transform.position, Quaternion.identity);
+        Invoke("RecursiveCreateApple", periodCreateApple);
+    }
+
+}
