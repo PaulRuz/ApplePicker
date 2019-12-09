eaf2629d9a59a76b1866a340284bcb2b3e6f0414
Pavel Ruzanov <ruzanov@yahoo.com>
2019-12-09 17:14:09 +0300

    Creating Basket and BasketManager

---
+++ Prototype/Assets/Scripts/Basket.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Basket : MonoBehaviour
+{
+    private string targetTag = "Apple";
+
+    private void OnMouseDrag()
+    {
+        Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 currentPosition = transform.position;
+        currentPosition.x = mousePosition.x;
+        transform.position = currentPosition;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject selectedApple = collision.gameObject;
+        if (selectedApple.CompareTag(targetTag))
+        {
+            Destroy(selectedApple);
+        }
+    }
+}
---
+++ Prototype/Assets/Scripts/BasketManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasketManager : MonoBehaviour
+{
+    public GameObject basketPrefab = null;
+    [Range(1, 3)] public byte countBaskets = 3;
+
+    private List<GameObject> basketList = null;
+
+    void Awake()
+    {
+        basketList = new List<GameObject>();
+
+        Vector2 pos = transform.position;
+        float spacingX = Mathf.Abs(pos.x);
+
+        for (int i = 0; i < countBaskets; i++)
+        {
+            GameObject newBasket = Instantiate(basketPrefab, pos, Quaternion.identity);
+            newBasket.transform.parent = transform;
+            pos.x = spacingX * i;
+
+            basketList.Add(newBasket);
+        }
+    }
+}
